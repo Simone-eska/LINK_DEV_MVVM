982f3aacd575b6e81c1f4b41318ae00fdf002538
Simone_EsKa <simsimaesmat@gmail.com>
2019-04-11 22:59:59 +0200

    appBar

--- app/src/main/java/com/example/viewmodeltest/home/view/HomeActivity.java
+++ app/src/main/java/com/example/viewmodeltest/home/view/HomeActivity.java
@@ -3,10 +3,7 @@ package com.example.viewmodeltest.home.view;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.TextView;
 
 import com.example.viewmodeltest.R;
 import com.example.viewmodeltest.base.BaseActivity;
@@ -34,13 +31,6 @@ public class HomeActivity extends BaseActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        ((SearchView)toolbar.findViewById(R.id.searchview)).setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean b) {
-                ((TextView)toolbar.findViewById(R.id.appbar_title_tv)).setVisibility(b?View.GONE:View.VISIBLE);
-            }
-        });
-
     }
 
 }
